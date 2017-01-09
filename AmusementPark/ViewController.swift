0c542709a5e0b7f51c3946fcba85aa4da33e2ebd
Andros Slowley <androsslowley@andross-macbook-pro.local>
2017-01-09 12:22:18 -0500

    ViewController Integration

--- AmusementPark/ViewController.swift
+++ AmusementPark/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func mainGuestButton() {
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
